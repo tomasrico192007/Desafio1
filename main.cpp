09b8be897ffb7e3f58c0ecab3872c4e28d0d8cdb
Tomas Rico <tomas.rico@udea.edu.co>
2025-09-27 17:26:27 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,59 +1,91 @@
 #include <iostream>
 #include <fstream>
-#include <iostream>
-#include <vector>
-#include <string>
-
 using namespace std;
 
-vector<unsigned char> descifrar(vector<unsigned char> datos, unsigned char clave) {
-    vector<unsigned char> resultado;
-    for (size_t i = 0; i < datos.size(); i++) {   // cambiado a size_t
-        resultado.push_back(datos[i] ^ clave);
+//Funciones
+
+unsigned char* leerarchivo(const char* nombre, int &tam) {
+    ifstream f(nombre, ios::binary);
+    if (!f.is_open()) {
+        cout << "Error, el archivo nos e pudo abrir" << nombre << endl;
+        tam = 0;
+        return NULL;
     }
-    return resultado;
+
+    f.seekg(0, ios::end);
+    tam = f.tellg();
+    f.seekg(0, ios::beg);
+
+    unsigned char* buffer = new unsigned char[tam];
+    f.read((char*)buffer, tam);
+    f.close();
+    return buffer;
 }
 
-string descomprimirRLE(vector<unsigned char> datos) {
-    string resultado;
-    for (size_t i = 0; i < datos.size(); i += 2) {  // cambiado a size_t
-        unsigned char caracter = datos[i];
-        int cantidad = datos[i + 1];
-        for (int j = 0; j < cantidad; j++) {
-            resultado += caracter;
-        }
-    }
-    return resultado;
+
+unsigned char rotar(unsigned char b, int rot) {
+    rot &= 7;
+    return (b >> rot) | (b << (8 - rot));
 }
 
-string descomprimirLZ78(vector<unsigned char> datos) {
-    string resultado;
-    vector<string> diccionario(1, "");
-    for (size_t i = 0; i < datos.size(); i += 2) {  // cambiado a size_t
-        int indice = datos[i];
-        char c = datos[i + 1];
-        string entrada = diccionario[indice] + c;
-        diccionario.push_back(entrada);
-        resultado += entrada;
+void descifrar(unsigned char* datos, int tam, int rot, unsigned char clave) {
+    for (int i = 0; i < tam; i++) {
+        unsigned char b = datos[i];
+        b = b ^ clave;
+        b = rotar(b, rot);
+        datos[i] = b;
     }
-    return resultado;
 }
 
+void mostrarbytes(const char* titulo, unsigned char* datos, int tam) {
+    cout << "Los" << titulo << " (primeros 50 bytes):\n";
+    for (int i = 0; i < tam && i < 50; i++) {
+        printf("%02X ", datos[i]);
+    }
+    cout << "\n";
+
+    cout << titulo << " como texto";
+    for (int i = 0; i < tam && i < 50; i++) {
+        char c = (datos[i] >= 32 && datos[i] <= 126) ? datos[i] : '.';
+        cout << c;
+    }
+}
+
+
+//Codigo main
 int main() {
-    vector<unsigned char> datos = {65, 3, 66, 2, 67, 1}; // Ejemplo RLE: AAA BB C
-    vector<unsigned char> datosLZ = {0, 'A', 0, 'B', 0, 'C', 1, 'B'};
+    const int n = 4;
+    const char* archivos[n] = {
+        "Encriptado1.txt",
+        "Encriptado2.txt",
+        "Encriptado3.txt",
+        "Encriptado4.txt"
+    };
 
-    unsigned char clave = 123;
+    int rotaciones[n] = {3, 3, 3, 3};
+    unsigned char claves[n] = {0x5A, 0x5A, 0x40, 0x40};
 
-    cout << "Descifrado: ";
-    vector<unsigned char> datosDescifrados = descifrar(datos, clave);
-    for (size_t i = 0; i < datosDescifrados.size(); i++) {   // cambiado a size_t
-        cout << datosDescifrados[i] << " ";
+    cout << "Se procesaran los siguientes " << n << "archivos" << endl;
+
+    for (int i = 0; i < n; i++) {
+        cout << "Procesando los archivos" << archivos[i] <<endl;
+
+        int tam = 0;
+        unsigned char* datos = leerarchivo(archivos[i], tam);
+        if (!datos || tam == 0) {
+            cout << "Hay un archivo vacio o hubo un erroa a la hora de leerlo" << endl;
+            continue;
+        }
+
+        mostrarbytes("El original", datos, tam);
+
+        descifrar(datos, tam, rotaciones[i], claves[i]);
+
+        mostrarbytes("El descifrado", datos, tam);
+
+        delete[] datos;
     }
-    cout << endl;
 
-    cout << "RLE: " << descomprimirRLE(datos) << endl;
-    cout << "LZ78: " << descomprimirLZ78(datosLZ) << endl;
-
+    cout << "Terminado";
     return 0;
 }
